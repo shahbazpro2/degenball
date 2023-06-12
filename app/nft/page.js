8673f5c04a280dacb231a00821f59e3a9e4c7fbe
shahbaz <shahbaz.pro2@gmail.com>
2023-06-13 02:10:41 +0500

    Add nft page

---
+++ app/nft/page.js
@@ -0,0 +1,12 @@
+import Nft from '@/components/nft'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <Nft />
+        </div>
+    )
+}
+
+export default page
